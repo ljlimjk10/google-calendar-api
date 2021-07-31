2c9082b8a196ee0b9170c586b51fb769e91d9368
Jun Kai <ljlimjk10@gmail.com>
2021-08-01 00:57:40 +0800

    Added CalendarEventUpdateSerializer

--- planner/serializers/event_serializers.py
+++ planner/serializers/event_serializers.py
@@ -5,34 +5,34 @@ from rest_framework import serializers
 from planner.models import GCalendar, GCalendarEvent
 from planner.models import User
 from rest_framework.generics import get_object_or_404
-from planner.utils.google_calendar_service import GoogleCalendar
+from planner.utils.google_calendar_service import GoogleCalendar, GoogleCalendarEvent
 
 
 class CalendarSerializer(serializers.ModelSerializer):
 
     gCalendarEvent = serializers.StringRelatedField(many=True)
-    email = serializers.CharField(max_length=200, write_only=True)
+    owner = serializers.ReadOnlyField(source='owner.email')
 
     class Meta:
         model = GCalendar
-        fields = ['calendarTitle','calendarId', 'gCalendarEvent', 'email']
+        fields = ['calendarTitle','calendarId', 'gCalendarEvent', 'owner']
         #COMMENT OUT TO IGNORE GOOGLE CALENDAR API
         # extra_kwargs = {"calendarId":{"read_only":True}}
-        extra_kwargs = {"createdBy":{"read_only":True}}
+        
     
     def create(self,validated_data):
         if self.is_valid():
             calendarTitle = validated_data.get("calendarTitle")
+            #COMMENT OUT calendarId when google api in use
             calendarId = validated_data.get("calendarId")
+            owner = validated_data.get("owner")
             #COMMENTED OUT TO IGNORE GOOGLE CALENDAR API 
-            # calendarId = GoogleCalendar.createCalendar(self,calendarTitle)
-            email = validated_data.get("email")
-            createdBy = get_object_or_404(User, email=email)
+            #calendarId = GoogleCalendar.createCalendar(self,calendarTitle)
             gCalendar = GCalendar.objects.create(
                 calendarTitle = calendarTitle,
                 calendarId = calendarId,
-                createdBy = createdBy
-            )
+                owner = owner
+                )
         return gCalendar
     
     def update(self,instance,validated_data):
@@ -47,17 +47,19 @@ class CalendarSerializer(serializers.ModelSerializer):
 class CalendarEventSerializer(serializers.ModelSerializer):
 
     calendarTitle = serializers.CharField(max_length=200, write_only=True)
-    email = serializers.CharField(max_length=200, write_only=True)
+    owner = serializers.ReadOnlyField(source='owner.email')
 
     class Meta:
         model = GCalendarEvent
         fields = "__all__"
-        extra_kwargs = {"createdBy":{"read_only":True}, "gCalendar":{"read_only":True}}
+        extra_kwargs = {"gCalendar":{"read_only":True}, "eventId":{"read_only":True}}
+        depth = 1
     
     def create(self,validated_data):
         if self.is_valid():
             calendarTitle = validated_data.get("calendarTitle")
             gCalendar = get_object_or_404(GCalendar, calendarTitle=calendarTitle)
+            gCalendar = validated_data.pop("gCalendar")
             eventTitle = validated_data.get("eventTitle")
             location = validated_data.get("location")
             description = validated_data.get("description")
@@ -65,8 +67,37 @@ class CalendarEventSerializer(serializers.ModelSerializer):
             startDateTimeIso = startDateTime.isoformat()
             endDateTime = validated_data.get("endDateTime")
             endDateTimeIso = endDateTime.isoformat()
-            email = validated_data.get("email")
-            createdBy = get_object_or_404(User, email=email)
-            gcalendarevent = GCalendarEvent.objects.create(gCalendar=gCalendar,eventTitle=eventTitle,location=location,description=description,startDateTime=startDateTimeIso,endDateTime=endDateTimeIso,createdBy=createdBy)
+            owner = validated_data.get("owner")
+            #COMMENTED OUT TO IGNORE GOOGLE CALENDAR API
+            # getCalendarId = GoogleCalendarEvent.getCalendarId(self, calendarTitle)
+            # eventId = GoogleCalendarEvent.createCalendarEvent(self,calendarTitle,eventTitle,location,description,startDateTime,endDateTime)
+            gcalendarevent = GCalendarEvent.objects.create(gCalendar=gCalendar,eventTitle=eventTitle,location=location,description=description,startDateTime=startDateTimeIso,endDateTime=endDateTimeIso,owner=owner)
+            #COMMENTED OUT TO IGNORE GOOGLE CALENDAR API
+            #gcalendarevent = GCalendarEvent.objects.create(gCalendar=gCalendar,eventId=eventId,eventTitle=eventTitle,location=location,description=description,startDateTime=startDateTimeIso,endDateTime=endDateTimeIso,createdBy=createdBy)
         return gcalendarevent
 
+class CalendarEventUpdateSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.email')
+
+    class Meta:
+        model = GCalendarEvent
+        fields = '__all__'
+        extra_kwargs = {"gCalendar":{"read_only":True}, "eventId":{"read_only":True}}
+        depth = 1
+
+    def update(self,instance,validated_data):
+        if self.is_valid():
+            instance.eventTitle = validated_data.get("eventTitle", instance.eventTitle)
+            instance.location = validated_data.get("location", instance.location)
+            instance.description = validated_data.get("description", instance.description)
+            instance.startDateTime = validated_data.get("startDateTime", instance.startDateTime)
+            instance.endDateTime = validated_data.get("endDateTime", instance.endDateTime)
+            instance.eventId = validated_data.get("eventId")
+            #COMMENTED OUT TO IGNORE GOOGLE CALENDAR API
+            # gcalendar = GCalendar.objects.first()
+            # calendar_id_field_name = "calendarId"
+            # calendarId = getattr(gcalendar,calendar_id_field_name)
+            # eventId = GoogleCalendarEvent.updateCalendarEvent(self,calendarId,instance.eventId,instance.eventTitle,instance.location,instance.description,instance.startDateTime,instance.endDateTime)
+            instance.save()
+            return instance
